d0656b8bd4aae9481f2e57eb5fdb1501b6b0f51c
root-naveen <ee.naveenchandra@gmail.com>
2024-01-06 17:55:37 +0530

    added LL

--- src/pp/Linkedlist/Node.java
+++ src/pp/Linkedlist/Node.java
@@ -1,5 +1,27 @@
 package pp.Linkedlist;
 
 public class Node {
+	private int data;
+	private Node next;
+	
+	//parameterized constructor
+	public Node(int data) {
+		this.data = data;
+	}
+	
+	//getter and setter
+	public int getData() {
+		return data;
+	}
+	public Node getNext() {
+		return next;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	public void setNext(Node node) {
+		this.next = node;
+	}
 
 }
